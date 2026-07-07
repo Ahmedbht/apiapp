785ce41143209e9e616995460c29058b1244cd5a
Ahmed <ahmedbouhtit8@gmail.com>
2026-07-07 13:45:12 +0100

    delete old db

--- app/app.py
+++ app/app.py
@@ -7,19 +7,6 @@ from app.database import engine ,get_db
 models.Base.metadata.create_all(bind=engine)
 app =FastAPI()
 
-text_posts = {
-    "1": {"title": "New Post", "content": "Cool test post"},
-    "2": {"title": "Python Tip", "content": "Use list comprehensions for cleaner loops."},
-    "3": {"title": "Daily Motivation", "content": "Consistency beats intensity every time."},
-    "4": {"title": "Fun Fact", "content": "The first computer bug was an actual moth found in a Harvard Mark II."},
-    "5": {"title": "Update", "content": "Just launched my new project! Excited to share more soon."},
-    "6": {"title": "Tech Insight", "content": "Async IO in Python can massively speed up I/O-bound tasks."},
-    "7": {"title": "Quote", "content": "Programs must be written for people to read, and only incidentally for machines."},
-    "8": {"title": "Weekend Plans", "content": "Might finally clean up my GitHub repos... or just play some Minecraft."},
-    "9": {"title": "Question", "content": "What's the most underrated Python library you've ever used?"},
-    "10": {"title": "Mini Announcement", "content": "New video drops tomorrow covering the weirdest Python features!"},
-}
-
 
 
 @app.get("/posts")
--- app/schemas.py
+++ app/schemas.py
@@ -3,3 +3,11 @@ from pydantic import BaseModel
 class PostCreate(BaseModel):
     title: str
     content: str
+class PostResponse(BaseModel):
+    id:int
+    title:str
+    content: str
+    created_at: datetime
+
+    class Config:
+        from_attributes=True
